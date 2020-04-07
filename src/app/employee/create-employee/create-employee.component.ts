6d36e1712b5d19abcee5f9125a70b9438f36f476
ja <jagoda.jarz@gmail.com>
2020-04-07 19:22:03 +0200

    use FormGroup

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl,FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
@@ -7,19 +7,48 @@ import {FormGroup, FormControl } from '@angular/forms';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  employeeForm: FormGroup; 
+  employeeForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
     this.employeeForm = new FormGroup({
       fullName: new FormControl(),
-      email: new FormControl()
+      email: new FormControl(),
+      //create skills form group
+      skills: new FormGroup({
+        skillName: new FormControl(),
+        experienceInYear: new FormControl(),
+        proficiency: new FormControl(),
+      })
     })
   }
 
-  onSubmit(){
+  onSubmit(): void {
     console.log(this.employeeForm.value);
+    // take a value
+    console.log(this.employeeForm.controls.fullName.value);
+    console.log(this.employeeForm.controls.email.value);
+    console.log(this.employeeForm.get('fullName').value);
+  }
+
+  onLoadData(): void {
+    // SOME VALUE
+    this.employeeForm.patchValue({
+      fullName: 'test',
+      email: 'email@cc.com'
+    });
+
+    // ALL VALUE
+    // this.employeeForm.setValue({
+    //   fullName: 'test',
+    //   email: 'email@cc.com',
+    //   skills: {
+    //     skillName: 'Angular',
+    //     experienceInYear: 0,
+    //     proficiency: 'begginer'
+    //   }
+    // });
   }
 
 
