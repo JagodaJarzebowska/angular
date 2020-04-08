ac6885cbecf4e8725fb8ce0ffd458ae900790c97
ja <jagoda.jarz@gmail.com>
2020-04-08 21:19:48 +0200

    Validators

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,FormBuilder } from '@angular/forms';
+import { FormGroup, Validators,FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
@@ -13,7 +13,7 @@ export class CreateEmployeeComponent implements OnInit {
 
   ngOnInit(): void {
     this.employeeForm = this.formBuilder.group({
-        fullName: [''],
+        fullName: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
         email: [''],
         skills: this.formBuilder.group({
           skillName: [''],
