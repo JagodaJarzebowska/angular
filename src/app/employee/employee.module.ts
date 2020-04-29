ad4640be9d9680bc8f98aec65d612f7d21b1e78d
ja <jagoda.jarz@gmail.com>
2020-04-29 19:33:41 +0200

    create employee separated module

---
+++ src/app/employee/employee.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
+
+@NgModule({
+  declarations: [
+    CreateEmployeeComponent,
+    ListEmployeeComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class EmployeeModule { }
