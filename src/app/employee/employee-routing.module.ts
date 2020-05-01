bbf23a790d986402df6c01af09976f1c894b08ff
ja <jagoda.jarz@gmail.com>
2020-05-01 14:57:36 +0200

    lazy load route - add prefix 'employees'

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -5,9 +5,11 @@ import { CreateEmployeeComponent } from './create-employee/create-employee.compo
 
 
 const routes: Routes = [
-  { path: 'list', component: ListEmployeeComponent },
-  { path: 'create', component: CreateEmployeeComponent },
-  { path: 'edit/:id', component: CreateEmployeeComponent },
+  { path: 'employees', children: [
+    { path: '', component: ListEmployeeComponent },
+    { path: 'create', component: CreateEmployeeComponent },
+    { path: 'edit/:id', component: CreateEmployeeComponent },
+  ]}
 ];
 
 @NgModule({
